a0c11f0ea5a6aec3a9882a1aca71732d95028136
sparkbold-git <sparkboldstudio@gmail.com>
2018-11-09 12:53:01 -0600

    add empty card

--- src/Components/DrawDeck.js
+++ src/Components/DrawDeck.js
@@ -1,20 +1,22 @@
 import React from "react";
-import Card from "./Card";
 
 export default class DrawDeck extends React.Component {
   render() {
     const { deck, drawCardClick, id } = this.props;
     // console.log("updatedDrawDeck==>", deck);
     return (
-      <div
-        className="card"
-        onClick={() => {
-          drawCardClick(deck[0], id);
-        }}
-      >
+      <div className="card">
         {deck[0] ? (
-          <img src={require("../images/card_back.png")} alt="oh!no" />
-        ) : null}
+          <img
+            onClick={() => {
+              drawCardClick(deck[0], id);
+            }}
+            src={require("../images/card_back.png")}
+            alt="oh!no"
+          />
+        ) : (
+          <img src={require(`../images/empty_card.png`)} />
+        )}
       </div>
     );
   }
--- src/Components/PlayerDeck.js
+++ src/Components/PlayerDeck.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Card from "./Card";
 
 export default class PlayerDeck extends React.Component {
   render() {
