6ca1641560a19a99d65d88752e8fae482a99a90e
sparkbold-git <sparkboldstudio@gmail.com>
2018-11-13 14:49:38 -0600

    ok

--- src/Containers/SignUp.js
+++ src/Containers/SignUp.js
@@ -1,16 +1,67 @@
-import React from 'react'
+import React from "react";
+import AuthService from "./AuthService";
 
 export default class SignUp extends React.Component {
-    render() {
-        return (
-            <div className='forms'>
-                <form>
-                    Username: <input type='text' />
-                    Password: <input type='password' />
-                    Password: <input type='password' />
-                    <button>Sign Up</button>
-                </form>
-            </div>
-        )
-    }
+  constructor() {
+    super();
+    this.state = { username: "", password: "" };
+  }
+
+  handleSubmit = e => {
+    const Auth = new AuthService();
+    e.preventDefault();
+    const username = this.state.username;
+    const password = this.state.password;
+
+    username &&
+      password &&
+      fetch("http://localhost:3000/api/v1/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json"
+        },
+        body: JSON.stringify({
+          user: {
+            username,
+            password
+          }
+        })
+      })
+        .then(r => this.props.history.replace("/game"))
+        .catch(err => {
+          alert(err);
+          this.props.history.replace("/signup");
+        });
+  };
+  handleChange = e => {
+    console.log(e.target.name, e.target.value);
+    this.setState({ [e.target.name]: e.target.value });
+  };
+  render() {
+    return (
+      <div className="center">
+        <div className="form">
+          <h1>Sign Up</h1>
+          <form onSubmit={this.handleSubmit}>
+            <input
+              className="form-item"
+              placeholder="Username goes here..."
+              name="username"
+              type="text"
+              onChange={this.handleChange}
+            />
+            <input
+              className="form-item"
+              placeholder="Password goes here..."
+              name="password"
+              type="password"
+              onChange={this.handleChange}
+            />
+            <button>Sign Up</button>
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
