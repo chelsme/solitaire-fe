344a879a709fc7a013fcc3693b332b793fba7b13
sparkbold-git <sparkboldstudio@gmail.com>
2018-11-13 17:35:19 -0600

    fix image src

--- src/Components/PlayerDeck.js
+++ src/Components/PlayerDeck.js
@@ -4,8 +4,10 @@ export default class PlayerDeck extends React.Component {
   render() {
     const { deck } = this.props;
     return (
-      <div id='playerDeck' className="card">
-        {deck[0] ? <img src={deck[0].image} alt="oh!no" /> : null}
+      <div id="playerDeck" className="card">
+        {deck[0] ? (
+          <img src={require(`../deck/${deck[0].code}.png`)} alt="oh!no" />
+        ) : null}
       </div>
     );
   }
--- src/Components/TableDeck.js
+++ src/Components/TableDeck.js
@@ -1,29 +1,38 @@
 import React from "react";
 
 export default class TableDeck extends React.Component {
-    render() {
-        const { deck, tableCardClick, id } = this.props;
-        const under = new Array(deck.value.length).fill(0);
-        return (
-            <div className="card">
-                {deck.value[0] ? (
-                    <div className='deck'>
-                        {under.map((fill, index) => {
-                            return <img key={index} className={"card" + index} src={require(`../images/card_back.png`)} alt='stop it' />
-                        })}
-                        <img className='topCard'
-                            onClick={() => {
-                                tableCardClick(deck, id);
-                            }}
-                            src={deck.value[0].image}
-                            alt="oh!no"
-                        />
-                    </div>
-
-                ) : (
-                        <img src={require(`../images/skull_${id + 1}.png`)} alt='no!' />
-                    )}
-            </div>
-        );
-    }
+  render() {
+    const { deck, tableCardClick, id } = this.props;
+    console.log(deck.value[0]);
+    const under = new Array(deck.value.length).fill(0);
+    // let img = require(deck.value[0].image);
+    return (
+      <div className="card">
+        {deck.value[0] ? (
+          <div className="deck">
+            {under.map((fill, index) => {
+              return (
+                <img
+                  key={index}
+                  className={"card" + index}
+                  src={require(`../images/card_back.png`)}
+                  alt="stop it"
+                />
+              );
+            })}
+            <img
+              className="topCard"
+              onClick={() => {
+                tableCardClick(deck, id);
+              }}
+              src={require(`../deck/${deck.value[0].code}.png`)}
+              alt="oh!no"
+            />
+          </div>
+        ) : (
+          <img src={require(`../images/skull_${id + 1}.png`)} alt="no!" />
+        )}
+      </div>
+    );
+  }
 }
